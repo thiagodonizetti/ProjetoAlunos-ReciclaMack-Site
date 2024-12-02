0fcca894ead5eab1570e8ced7261b3af222ecf43
Thiago Donizetti dos Santos <thiagods05@gmail.com>
2024-12-02 13:41:35 -0300

    files upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Código JavaScript para futura integração com a API do YouTube e Instagram
+    console.log("Página de resultados carregada.");
+});
